b26eaaec6b386f325d13eb25a75d5b4b9fed5b0b
Amir.haf76 <amir.haf76@gmail.com>
2025-07-12 11:01:44 +0330

    Experiment some features and method.

--- src/Editor/Program.cs
+++ src/Editor/Program.cs
@@ -1,7 +1,9 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CatCMS;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +11,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 
 
+builder.Services
+    .AddHttpClient("WebAPI", client => client.BaseAddress = new Uri("https://api.contoso.com/v1.0"))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp
+    .GetRequiredService<IHttpClientFactory>()
+    .CreateClient("WebAPI"));
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
